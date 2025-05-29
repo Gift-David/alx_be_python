49aedcd8238db63a4e44d85945899fd2bf1d5ea2
David Gift <giftdavid189@gmail.com>
2025-05-29 11:38:46 +0100

    Made changes to the math_case_calculator.py file

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,7 +1,7 @@
 # A script that uses match case to perfom simple arithmetic operations between 2 numbers
 num1 = int(input("Enter the first number:"))
 num2 = int(input("Enter the second number:"))
-operation = input("Enter an operation ( +, -, *, /):")
+operation = input("Choose the operation operation ( +, -, *, /):")
 
 match operation:
     case "+":
