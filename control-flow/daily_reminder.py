5f8685a11465fdc690725ffb69407b5538b7218c
David Gift <giftdavid189@gmail.com>
2025-06-01 19:01:19 +0100

    Edited the control-flow directory

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,8 +1,8 @@
 # A script that remind users about a single, priority task for the day based on time sensitivity
 
-task = input("Enter your task description: ").lower()
-priority = input("Task's priority (high, medium, low): ").lower()
-time_bound = input("Is it time-bound? (yes or no): ").lower()
+task = input("Enter your task description:").lower()
+priority = input("Task's priority (high, medium, low):").lower()
+time_bound = input("Is it time-bound? (yes or no):").lower()
 
 match priority:
     case "high":
