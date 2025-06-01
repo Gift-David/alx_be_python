8eda67e6ed8a0afa5da7f6308721e181432768b2
David Gift <giftdavid189@gmail.com>
2025-06-01 20:00:05 +0100

    added the pattern.py file

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,5 +1,9 @@
 # A script that uses nested loops to print a square pattern pased on as positive integer
 
 integer = int(input("Enter the size of the pattern:"))
-
-while 
+row = 0
+while row < integer:
+    for i in range(integer):
+        print("*", end="")
+    print()
+    row += 1
