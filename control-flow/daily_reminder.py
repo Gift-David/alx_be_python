40405561412e74995d2963ffe470eac912cb1c09
David Gift <giftdavid189@gmail.com>
2025-06-03 10:01:44 +0100

    edited the daily_reminder.py file

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,25 +1,28 @@
 # A script that remind users about a single, priority task for the day based on time sensitivity
 
-task = input("Enter your task:")
-time_bound = input("Is it time-bound? (yes/no):")
-priority = input("Priority (high/medium/low):")
-
+task = input("Enter your task:").title()
+time_bound = input("Is it time-bound? (yes/no):").lower()
+priority = input("Priority (high/medium/low):").lower()
 
 match priority:
     case "high":
-        reminder ="is a high priority task"
+        reminder = f"Reminder: {task} is a {priority} priority task"
     case "medium":
-        reminder ="is a medium priority task"
+        reminder = f"{task} is a medium priority task"
     case "low":
-        reminder ="is a low priority task"
+        reminder = f"{task} is a low priority task"
     case _:
         print("Invalid priority level entered")
 
 if time_bound == "yes":
-    time_reminder = "that requires immediate attention today"
+    reminder += "that requires immediate attention today"
 elif time_bound == "no":
-    time_reminder = ". Consider completing it when you have free time."
+    reminder += ". Consider completing it when you have free time."
 else:
     print("Invalid input for time-bound")
 
-print(f"{task} {reminder}{time_reminder}")
+print(reminder)
+
+
+
+# print(f"{task} {reminder}{time_reminder}")
