ce277d4a096cbb874651f0caa369214888e5cc16
David Gift <giftdavid189@gmail.com>
2025-06-05 14:09:04 +0100

    added the temp_conversion_tool.py file

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,7 +1,7 @@
 # A script that converts temperature from celsius to fahrenheit and vice versa
 
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 /5
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 
 def convert_to_celsius(fahrenheit):
     return (fahrenheit - 32 ) * FAHRENHEIT_TO_CELSIUS_FACTOR
