7000f9164d4522d76242d65d7caaf5eccc8b0c60
David Gift <giftdavid189@gmail.com>
2025-06-13 15:08:25 +0100

    Corrected the bank_account.py file

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -16,4 +16,4 @@ class BankAccount:
             return True
         
     def display_balance(self):
-        return self.account_balance
+        return f"Current Balance: {self.account_balance}"
