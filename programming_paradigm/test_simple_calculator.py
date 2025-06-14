fc74181ee3436104f0d20cfad23be6d9a98e50b0
David Gift <giftdavid189@gmail.com>
2025-06-14 15:52:10 +0100

    Edited test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -5,13 +5,14 @@ from simple_calculator import SimpleCalculator
 class Test(unittest.TestCase):
 
     def test_add(self):
-        self.assertAlmostEqual(SimpleCalculator.add(self, 5,5), 10)
+        self.assertEqual(SimpleCalculator.add(self, 5,5), 10)
 
     def test_subtract(self):
-        self.assertAlmostEqual(SimpleCalculator.subtract(self, 10,5), 5)
+        self.assertEqual(SimpleCalculator.subtract(self, 10,5), 5)
 
     def test_multiply(self):
-        self.assertAlmostEqual(SimpleCalculator.multiply(self, 10,5), 50)
+        self.assertEqual(SimpleCalculator.multiply(self, 10,5), 50)
 
     def test_divide(self):
-        self.assertAlmostEqual(SimpleCalculator.divide(self, 20,5), 4)
+        self.assertEqual(SimpleCalculator.divide(self, 20,5), 4)
+        self.assertEqual(SimpleCalculator.divide(self, 20,0), ZeroDivisionError())
