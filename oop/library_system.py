b26aba69d42453b2d41e2e4260aa4ab0d3d10057
David Gift <giftdavid189@gmail.com>
2025-06-16 17:37:59 +0100

    Edited the library_management.py file

--- oop/library_system.py
+++ oop/library_system.py
@@ -18,7 +18,7 @@ class EBook(Book):
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
-        self.page_coount = page_count
+        self.page_count = page_count
 
     def __str__(self):
         return f"{self.title}, {self.author}, {self.page_coount}"
